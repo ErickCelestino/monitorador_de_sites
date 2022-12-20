ff71492acc7e4d023036ce0ffb95662f48ac0183
PuraPreguica <erickcelestimo@gmail.com>
2022-12-20 00:03:12 -0300

    Implementando novas funcionalidades

--- hello/hello.go
+++ hello/hello.go
@@ -2,19 +2,53 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Douglas"
 	versao := 1.1
 
 	fmt.Println("Olá se.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
-	var comando int
-	fmt.Scanf("%d", &comando)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+
+	fmt.Println("O comando escolhido foi", comandoLido)
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	//site := "https://www.cocamar.com.br"
+	//resp, err := http.Get(site)
 
 }
